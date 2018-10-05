842659e47d41687b185ee954cce9c861323780a1
Ben Layer <ben@studiorotate.com>
2018-10-05 12:03:13 +0100

    Add postcss import and set build setting for css and es-linting

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,12 +21,16 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: false,
 
   /*
   ** Global CSS
   */
   css: [
+    {
+      lang: "postcss",
+      src : "@/assets/css/main.css"
+    }
   ],
 
   /*
@@ -48,16 +52,8 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+    postcss: [
+      require("postcss-import")(),
+    ]
   }
 }
