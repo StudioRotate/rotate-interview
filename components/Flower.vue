54d4d5f4a34ea50a61d9bdb8d61903cf50288c23
Ben <ben.challenor@btopenworld.com>
2018-10-09 11:31:32 +0100

    Rename classes to Picker & add media query for mobile

--- components/Flower.vue
+++ components/Flower.vue
@@ -1,22 +1,44 @@
 <template lang="html">
-  <div class="container">
-
-  <div class="nav"></div>
-
-  <div class="main">
-    <h1>test</h1>
-
+  <div class="Picker">
+    <div class="Picker__preview">
+      <img v-for="(img, i) in imgs" :key="i" :src="`imgs/home_product_flower_${img}`" alt="" class="Picker__preview__flower">
+    </div>
   </div>
-
-  <div class="footer"></div>
-</div>
 </template>
 
 <script>
 export default {
-  name: "Flower"
+  name: "Flower",
+  data() {
+    return {
+      imgs: [
+        '1.png',
+        '2.png',
+        '3.png',
+      ]
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style>
+
+body {
+  background-color: #F1EEE5;
+}
+
+.Picker__preview {
+  width: 100%;
+  margin: auto;
+}
+
+@media(min-width: 800px) {
+  .Picker__preview {
+    width: 50%;
+  }
+}
+
+.Picker__preview__flower {
+  width: calc(100% / 3);
+}
 </style>
