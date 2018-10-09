15953a91ab848a10840b3679c743fc2087f40edc
Jim Tattersall <jim@studiorotate.com>
2018-10-09 13:36:37 +0100

    Add swiper functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,55 @@
 <template>
-  <Intro />
+  <div>
+    <Carousel />
+      <div class="Slider">
+      <div class="Slider__copy">
+        <h1>blah blah</h1>
+      </div>
+        <swiper :options="swiperOption" ref="mySwiper">
+        <div class="Slider__images">
+          <swiper-slide> <FirstImage /></swiper-slide>
+          <swiper-slide> slide 3</swiper-slide>
+          <swiper-slide> slide 2</swiper-slide>
+        </div>
+        </swiper>
+      </div>
+  </div>
 </template>
 
 <script>
-import Intro from '@/components/Intro.vue'
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import Carousel from '@/components/Carousel.vue'
+import FirstImage from '@/components/FirstImage.vue'
 
 export default {
   components: {
-    Intro
+    Carousel,
+    FirstImage,
+    swiper,
+    swiperSlide
   }
 }
 </script>
+
+<style>
+.Slider {
+  display: flex;
+  flex: row nowrap;
+  width: 100%;
+  height: 30%;
+}
+
+.Slider__copy {
+  width: 33%;
+  height: 50%;
+}
+
+.Slider__images {
+  width: 66%;
+}
+
+.Slider img {
+  width: 50%;
+}
+</style>
