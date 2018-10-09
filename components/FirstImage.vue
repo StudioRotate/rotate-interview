5cdb3543bd2f81917c1ebb74c866eb7596bc26c5
Jim Tattersall <jim@studiorotate.com>
2018-10-09 13:35:24 +0100

    Create image component

---
+++ components/FirstImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Image">
+    <img src="/imgs/home_carousel_1.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstImage'
+}
+
+</script>
+
+<style>
+</style>
